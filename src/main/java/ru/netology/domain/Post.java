1f7c6a732bc36ae201c4b84281618daac7e9a03e
kokanoka <kokanoka30@gmail.com>
2021-09-10 18:46:18 +0300

    add Classes for Post VK

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,77 @@
+package ru.netology.domain;
+
+public class Post {
+    private String id;
+    private String text;
+    private String dateTime;
+    private String headline;
+    private String logoImageUrl;
+    private String imageUrl;
+    private int viewNumber;
+    private int likeCounter;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getLogoImageUrl() {
+        return logoImageUrl;
+    }
+
+    public void setLogoImageUrl(String logoImageUrl) {
+        this.logoImageUrl = logoImageUrl;
+    }
+
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getViewNumber() {
+        return viewNumber;
+    }
+
+    public void setViewNumber(int viewNumber) {
+        this.viewNumber = viewNumber;
+    }
+
+    public int getLikeCounter() {
+        return likeCounter;
+    }
+
+    public void setLikeCounter(int likeCounter) {
+        this.likeCounter = likeCounter;
+    }
+}
