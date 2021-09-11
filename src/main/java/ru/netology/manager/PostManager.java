ef71f7885b17e1dbf69e48bfe05dd193021077ab
kokanoka <kokanoka30@gmail.com>
2021-09-11 14:19:19 +0300

    add package manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,16 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset, boolean extended, String fields) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+}
+
+
